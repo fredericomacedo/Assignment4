e353461a61b9870b636f78421cc8c83b44a7c566
Frederico Lúcio Macedo <fredvalentim.macedo@gmail.com>
2023-08-08 21:29:59 -0400

    correct project from Natalia

---
+++ rest-acmecollege/src/main/java/acmecollege/entity/AcademicStudentClub_.java
@@ -0,0 +1,9 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2023-08-08T19:13:13.256-0400")
+@StaticMetamodel(AcademicStudentClub.class)
+public class AcademicStudentClub_ extends StudentClub_ {
+}
---
+++ rest-acmecollege/src/main/java/acmecollege/entity/NonAcademicStudentClub_.java
@@ -0,0 +1,9 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2023-08-08T19:13:13.305-0400")
+@StaticMetamodel(NonAcademicStudentClub.class)
+public class NonAcademicStudentClub_ extends StudentClub_ {
+}
