fa817fc639b5909ca1ce43c937d68ddb2e650f99
danielbasz <danielbassporto@gmail.com>
2023-08-13 16:47:41 -0400

    Junit tests updated
    Coming from nat branch

--- REST-ACMECollege-Skeleton 2/src/test/java/acmecollege/entity/ClubMembershipTest.java
+++ REST-ACMECollege-Skeleton 2/src/test/java/acmecollege/entity/ClubMembershipTest.java
@@ -1,66 +1,80 @@
 package acmecollege.entity;
 
+import static acmecollege.utility.MyConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.Test;
+import java.lang.invoke.MethodHandles;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import acmecollege.MyObjectMapperProvider;
 
-import java.time.LocalDateTime;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class ClubMembershipTest {
 
-    @Test
-    public void testSetAndGetStudentClub() {
-        ClubMembership clubMembership = new ClubMembership();
-        StudentClub club = new AcademicStudentClub();
-        clubMembership.setStudentClub(club);
+    private static final Class<?> _thisClaz = MethodHandles.lookup().lookupClass();
+    private static final Logger logger = LogManager.getLogger(_thisClaz);
 
-        assertEquals(club, clubMembership.getStudentClub());
+    static final String HTTP_SCHEMA = "http";
+    static final String HOST = "localhost";
+    static final int PORT = 8080;
+    static URI uri;
+    static HttpAuthenticationFeature adminAuth;
+    static HttpAuthenticationFeature userAuth;
+    static int record_id = 1;
+
+    @BeforeAll
+    public static void oneTimeSetUp() throws Exception {
+        logger.debug("oneTimeSetUp");
+        uri = UriBuilder
+                .fromUri(APPLICATION_CONTEXT_ROOT + APPLICATION_API_VERSION)
+                .scheme(HTTP_SCHEMA)
+                .host(HOST)
+                .port(PORT)
+                .build();
+        adminAuth = HttpAuthenticationFeature.basic(DEFAULT_ADMIN_USER, DEFAULT_ADMIN_USER_PASSWORD);
+        userAuth = HttpAuthenticationFeature.basic(DEFAULT_USER, DEFAULT_USER_PASSWORD);
+    }
+
+    protected WebTarget webTarget;
+
+    @BeforeEach
+    public void setUp() {
+        Client client = ClientBuilder.newClient(
+                new ClientConfig().register(MyObjectMapperProvider.class).register(new LoggingFeature()));
+        webTarget = client.target(uri);
     }
 
     @Test
-    public void testSetAndGetCard() {
-        ClubMembership clubMembership = new ClubMembership();
-        MembershipCard card = new MembershipCard(); // assuming constructor
-        clubMembership.setCard(card);
-
-        assertEquals(card, clubMembership.getCard());
+    @Order(1)
+    public void test02_getAllClubMemberships_user() {
+        Response response = webTarget
+                .register(userAuth)
+                .path("clubmembership")
+                .request()
+                .get();
+        assertEquals(response.getStatus(), 403);
     }
 
     @Test
-    public void testSetAndGetDurationAndStatus() {
-        ClubMembership clubMembership = new ClubMembership();
-        DurationAndStatus durationAndStatus = new DurationAndStatus();
-        LocalDateTime startDate = LocalDateTime.now();
-        LocalDateTime endDate = startDate.plusDays(1);
-        durationAndStatus.setStartDate(startDate);
-        durationAndStatus.setEndDate(endDate);
-        durationAndStatus.setActive((byte) 1);
-
-        clubMembership.setDurationAndStatus(durationAndStatus);
-
-        assertEquals(durationAndStatus, clubMembership.getDurationAndStatus());
-    }
-
-    @Test
-    public void testEqualsAndHashCode() {
-        ClubMembership clubMembership1 = new ClubMembership();
-        ClubMembership clubMembership2 = new ClubMembership();
-
-        assertEquals(clubMembership1, clubMembership1); // reflexive
-        assertEquals(clubMembership1, clubMembership2); // symmetric
-        assertEquals(clubMembership1.hashCode(), clubMembership2.hashCode());
-
-        DurationAndStatus durationAndStatus = new DurationAndStatus();
-        LocalDateTime startDate = LocalDateTime.now();
-        LocalDateTime endDate = startDate.plusDays(1);
-        durationAndStatus.setStartDate(startDate);
-        durationAndStatus.setEndDate(endDate);
-        durationAndStatus.setActive((byte) 1);
-
-        clubMembership1.setDurationAndStatus(durationAndStatus); // assuming it affects identity
-
-        assertNotEquals(clubMembership1, clubMembership2);
-        assertNotEquals(clubMembership1.hashCode(), clubMembership2.hashCode());
+    @Order(2)
+    public void test03_getClubMembershipById_admin() {
+        Response response = webTarget
+                .register(adminAuth)
+                .path("clubmembership/1")
+                .request()
+                .get();
+        assertEquals(response.getStatus(), 200);
     }
 }
--- REST-ACMECollege-Skeleton 2/src/test/java/acmecollege/entity/StudentTest.java
+++ REST-ACMECollege-Skeleton 2/src/test/java/acmecollege/entity/StudentTest.java
@@ -1,21 +1,158 @@
 package acmecollege.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 import acmecollege.entity.Student;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.junit.jupiter.api.*;
+import acmecollege.MyObjectMapperProvider;
 
-class StudentTest {
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.lang.invoke.MethodHandles;
+import java.net.URI;
+
+import static acmecollege.utility.MyConstants.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StudentTest {
+
+    private static final Class<?> _thisClaz = MethodHandles.lookup().lookupClass();
+    private static final Logger logger = LogManager.getLogger(_thisClaz);
+    static final String HTTP_SCHEMA = "http";
+    static final String HOST = "localhost";
+    static final int PORT = 8080;
+    static URI uri;
+    static HttpAuthenticationFeature adminAuth;
+    static HttpAuthenticationFeature userAuth;
+    static int record_id = 2;
+
+    @BeforeAll
+    public static void oneTimeSetUp() throws Exception {
+        logger.debug("oneTimeSetUp");
+        uri = UriBuilder
+                .fromUri(APPLICATION_CONTEXT_ROOT + APPLICATION_API_VERSION)
+                .scheme(HTTP_SCHEMA)
+                .host(HOST)
+                .port(PORT)
+                .build();
+        adminAuth = HttpAuthenticationFeature.basic(DEFAULT_ADMIN_USER, DEFAULT_ADMIN_USER_PASSWORD);
+        userAuth = HttpAuthenticationFeature.basic(DEFAULT_USER, DEFAULT_USER_PASSWORD);
+    }
+
+    protected WebTarget webTarget;
+
+    @BeforeEach
+    public void setUp() {
+        Client client = ClientBuilder.newClient(
+                new ClientConfig().register(MyObjectMapperProvider.class).register(new LoggingFeature()));
+        webTarget = client.target(uri);
+    }
+
+    @Order(1)
     @Test
-    void testStudent() {
-        Student student = new Student();
-        student.setFirstName("John");
-        student.setLastName("Doe");
-        assertEquals("John", student.getFirstName());
-        assertEquals("Doe", student.getLastName());
+    public void test01_getAllStudents_admin() throws JsonMappingException, JsonProcessingException {
+        Response response = webTarget
+                .register(adminAuth)
+                .path("student")
+                .request()
+                .get();
+        assertEquals(response.getStatus(), 200);
+    }
+    @Order(2)
+    @Test
+    public void test02_getAllStudents_user() throws JsonMappingException, JsonProcessingException {
+        Response response = webTarget
+                .register(userAuth)
+                .path("student")
+                .request()
+                .get();
+        assertEquals(response.getStatus(), 403);
+    }
+    @Order(3)
+    @Test
+    public void test03_getStudentById_admin() throws JsonMappingException, JsonProcessingException {
+        Response response = webTarget
+                .register(adminAuth)
+                .path("student/1")
+
+                .request()
+                .get();
+        assertEquals(response.getStatus(), 200);
+    }
+    @Order(4)
+    @Test
+    public void test04_getStudentById_user() throws JsonMappingException, JsonProcessingException {
+        Response response = webTarget
+                .register(userAuth)
+                .path("student/1")
+                .request()
+                .get();
+        assertEquals(response.getStatus(), 200);
     }
     
-    // More tests for the other methods and properties can be added
+//    @Order(5)
+//    @Test
+//    public void test05_postStudent_admin() throws JsonMappingException, JsonProcessingException {
+//        Student student = new Student();
+//        student.setFirstName("John");
+//        student.setLastName("smith");
+//
+//        try (Response response = webTarget
+//                .register(adminAuth)
+//                .path("student")
+//                .request()
+//                .post(Entity.json(student))) {
+//            assertEquals(response.getStatus(), 200);
+//        }
+//    }
+    
+    @Order(5)
+    @Test
+    public void test06_postStudent_user() throws JsonMappingException, JsonProcessingException {
+        Student student = new Student();
+        student.setFullName("Jane","Doe");
+        try (Response response = webTarget
+                .register(userAuth)
+                .path("student")
+                .request()
+                .post(Entity.json(student))) {
+            assertEquals(response.getStatus(), 403);
+        }
+    }
+    
+    @Order(6)
+    @Test
+    public void test07_deleteStudent_admin() throws JsonMappingException, JsonProcessingException {
+        Response response = webTarget
+                .register(adminAuth)
+                .path("student/2")
+                .request()
+                .delete();
+        assertEquals(response.getStatus(), 200);
+    }
+
+    @Order(7)
+    @Test
+    public void test08_deleteStudent_user() throws JsonMappingException, JsonProcessingException {
+        try (Response response = webTarget
+                .register(userAuth)
+                .path("student/{id}")
+                .resolveTemplate("id", 1)
+                .request()
+                .delete()) {
+            assertEquals(response.getStatus(), 403);
+        }
+    
+    }
 }
